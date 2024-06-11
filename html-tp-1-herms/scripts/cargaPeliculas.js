083313a647b968ba7917b7e512f5ea6cc995a4f4
Alejandro Hugo Herms <hermsale@gmail.com>
2024-06-11 11:52:32 -0300

    se esta trabajando en que las cosas se carguen por medio de un json

--- html-tp-1-herms/scripts/cargaPeliculas.js
+++ html-tp-1-herms/scripts/cargaPeliculas.js
@@ -1,12 +1,42 @@
-fetch('peliculas.json')
-      .then(response => response.json())
-      .then(data => {
-        // Iterar sobre cada película y crear las cartas de películas
-        data.forEach(pelicula => {
-          const cartaPelicula = crearCartaPelicula(pelicula);
-          contenedorPeliculas.appendChild(cartaPelicula);
-          console.log(pelicula);
-        });
-      })
-      .catch(error => console.error('Error al cargar el archivo JSON:', error));
-git
+const peliculas = `[
+    {
+      "titulo": "Spider Man",
+      "descripcion": "Una película sobre el superhéroe Spider-Man.",
+      "foto": "img/peliculas series/spiderman.png"
+    },
+    {
+      "titulo": "Rambo",
+      "descripcion": "Un exsoldado toma justicia por su cuenta.",
+      "foto": "img/peliculas series/rambo.png"
+    },
+    {
+      "titulo": "Armageddon",
+      "descripcion": "Un equipo intenta salvar la Tierra de un asteroide.",
+      "foto": "/img/peliculas series/armageddon.png"
+    }
+  ]`
+
+
+const jsonData = JSON.parse(peliculas);
+console.log(jsonData[0].titulo);
+// Obtener el contenedor donde se agregarán las tarjetas de películas
+const contenedorPeliculas = document.getElementById('div__seguirViendo');
+
+// Iterar sobre cada película en el archivo JSON
+jsonData.forEach(pelicula => {
+  // Generar la estructura HTML de la tarjeta de película
+  const tarjetaHTML = `
+    <div class="card card-seguir-viendo col-3 p-0">
+      <div class="card-seguir-viendo__loading"></div>
+      <img src="${pelicula.foto}" class="card-img-top" alt="...">
+      <div class="card-body p-0 m-0">
+        <p class="card-title p__cardSeguirViendo--img">
+          <img src="./img/icon/disney-icon.png" class="" style="width: 40%" alt="disney">
+        </p>
+      </div>
+    </div>
+  `;
+
+  // Agregar la tarjeta al contenedor
+  contenedorPeliculas.innerHTML += tarjetaHTML;
+});
